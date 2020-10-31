8e427f3b50097e20e132d192aa3701fe4e30cb98
ddb-jespindola <jespindola@ddb.com.co>
2020-10-31 12:57:59 -0500

    Change 404 page styles

--- footer.php
+++ footer.php
@@ -90,7 +90,7 @@
     </script>
 </footer>
 
-<!--Start of Tawk.to Script-->
+<!-- Start of Tawk.to Script
 <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
@@ -101,7 +101,7 @@ s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
-</script>
+</script> -->
 <!--End of Tawk.to Script-->
 
 </body>

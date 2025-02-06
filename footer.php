35ac3e499fa19e0b29206a5775e40f1556d583fd
jaw7388 <johnalexandere@hotmail.com>
2025-02-06 12:25:26 -0500

    Quitar info de medicamentos del footer

--- footer.php
+++ footer.php
@@ -17,34 +17,31 @@
 
     <div class="container">
         <div class="row">
-            
+
             <address class="col-md-4">
                 <h5>Contacto</h5>
                 <ul>
-                    <li><span class="ddb-icon icon-location-white"></span><a href="https://www.google.com/maps/place/Dep%C3%B3sito+de+Drogas+Boyac%C3%A1/@4.6860751,-74.1213518,15z/data=!4m2!3m1!1s0x0:0x11fb3f84b9c80ffb?sa=X&ved=2ahUKEwilhevoqa_rAhUCjlkKHS7VAxgQ_BIwCnoECBUQCA" target="_blank"  rel="noreferrer">
-                            Transversal 93 # 51-98 
-                            Parque Empresarial Puerta del Sol Bodega 18  . Bogotá-Colombia</a>
+                    <li><span class="ddb-icon icon-location-white"></span><a
+                            href="https://www.google.com/maps/place/Dep%C3%B3sito+de+Drogas+Boyac%C3%A1/@4.6860751,-74.1213518,15z/data=!4m2!3m1!1s0x0:0x11fb3f84b9c80ffb?sa=X&ved=2ahUKEwilhevoqa_rAhUCjlkKHS7VAxgQ_BIwCnoECBUQCA"
+                            target="_blank" rel="noreferrer">
+                            Transversal 93 # 51-98
+                            Parque Empresarial Puerta del Sol Bodega 18 . Bogotá-Colombia</a>
                     </li>
                     <li><span class="ddb-icon icon-clock-white"></span>Lun-Vie 8am-5pm | Sab 8am-1pm</li>
-                    <li><span class="ddb-icon icon-phone-white"></span><a href="tel:(+571)7432597">(601) 743 25 97</a></li>
-                    <li><span class="ddb-icon icon-phone-white"></span><a href="tel:+573125237420">(+57) 312 523 74 20</a></li>
-                    <li><span class="ddb-icon icon-phone-white"></span><a href="tel:(+57)3007484272">(+57) 300 748 42 72</a></li>
-                    <li><span class="ddb-icon icon-mail-white"></span><a href="mailto:info@ddb.com.co">info@ddb.com.co</a></li>
+                    <li><span class="ddb-icon icon-phone-white"></span><a href="tel:(+571)7432597">(601) 743 25 97</a>
+                    </li>
+                    <li><span class="ddb-icon icon-phone-white"></span><a href="tel:+573125237420">(+57) 312 523 74
+                            20</a></li>
+                    <li><span class="ddb-icon icon-phone-white"></span><a href="tel:(+57)3007484272">(+57) 300 748 42
+                            72</a></li>
+                    <li><span class="ddb-icon icon-mail-white"></span><a
+                            href="mailto:info@ddb.com.co">info@ddb.com.co</a></li>
                 </ul>
-    </address>
-            
+            </address>
+
             <div class="col-md-4">
                 <h5>Acceso rápido</h5>
                 <ul>
-                    <li>
-                        <a href="/categoria-producto/medicamento/">Medicamentos</a>
-                    </li>
-                    <li>
-                        <a href="/categoria-producto/medico-quirurgicos-2/">Dispositivos médicos</a>
-                    </li>
-                    <li>
-                        <a href="/categoria-producto/kits-de-atencion-social-institucional/">Kits de atención social institucional</a>
-                    </li>
                     <li>
                         <a href="/contacto/">Contacto</a>
                     </li>
@@ -63,18 +60,20 @@
             <div class="col-md-4">
                 <h5>Certificaciones</h5>
                 <div class="footer-logo">
-                        <!-- <img src="/wp-content/uploads/2020/06/Bureau-veritas-C0240563.png" alt="" class="footer-logo"> -->
-                        <ul><img src="/wp-content/uploads/2020/11/LOGO_BUREAU_VERITAS_DDB.jpg" alt="" class="footer-logo"  loading="lazy"> </ul>
+                    <!-- <img src="/wp-content/uploads/2020/06/Bureau-veritas-C0240563.png" alt="" class="footer-logo"> -->
+                    <ul><img src="/wp-content/uploads/2020/11/LOGO_BUREAU_VERITAS_DDB.jpg" alt="" class="footer-logo"
+                            loading="lazy"> </ul>
                 </div>
             </div>
 
         </div>
         <div class="row ">
             <div class="col-md-12 text-center social-footer">
-            <a href="https://www.linkedin.com/company/dep%C3%B3sito-de-drogas-boyac%C3%A1/" target="_blank"  rel="noreferrer">
-                <!-- <i class="fab fa-linkedin"></i> -->
-                <span class="ddb-icon icon-linkedin-white m-auto social-button"></span>
-            </a>
+                <a href="https://www.linkedin.com/company/dep%C3%B3sito-de-drogas-boyac%C3%A1/" target="_blank"
+                    rel="noreferrer">
+                    <!-- <i class="fab fa-linkedin"></i> -->
+                    <span class="ddb-icon icon-linkedin-white m-auto social-button"></span>
+                </a>
             </div>
         </div>
         <div class="row footer-copyright">
@@ -82,30 +81,33 @@
                 © Copyright 2020 | DEPÓSITO DE DROGAS BOYACÁ | TODOS LOS DERECHOS RESERVADOS
             </div>
         </div>
-    </div>  
-    
+    </div>
+
     <!-- AOS scroll animations -->
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script>
-        AOS.init({
-            once: true
-        });
+    AOS.init({
+        once: true
+    });
     </script>
 </footer>
 
 <!-- Start of Tawk.to Script -->
 <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5f3015a55c885a1b7fb78fee/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
+var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date();
+(function() {
+    var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/5f3015a55c885a1b7fb78fee/default';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
 })();
 </script>
 <!--End of Tawk.to Script-->
 
 </body>
+
 </html>
